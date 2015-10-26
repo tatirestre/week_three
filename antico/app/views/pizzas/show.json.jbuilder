826747ed18117815c62f9727113b741770670450
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-10-26 16:55:08 -0400

    Plus Antico

---
+++ antico/app/views/pizzas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @pizza, :id, :name, :description, :price, :created_at, :updated_at
---
+++ antico/db/migrate/20151026192141_create_pizzas.rb
@@ -0,0 +1,11 @@
+class CreatePizzas < ActiveRecord::Migration
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
+end
